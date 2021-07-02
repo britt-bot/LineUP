17e33191d70347aa79f5b55a9fa7327de464b5ff
Katherine Jackson <katherine.jackson206@gmail.com>
2021-07-01 19:45:29 -0500

    made homepage load

--- lineup/src/components/Navbar/index.js
+++ lineup/src/components/Navbar/index.js
@@ -15,28 +15,13 @@ function Navbar() {
             <Link
               to="/"
               className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
+                window.location.pathname === "/" ||
+                window.location.pathname === "/homepage"
                   ? "nav-link active"
                   : "nav-link"
               }
             >
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/discover"
-              className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
-            >
-              Discover
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/search"
-              className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
-            >
-              Search
+              Homepage
             </Link>
           </li>
         </ul>
