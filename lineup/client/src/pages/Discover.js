4c5154323fabcceac6b770a6c8006aa90638503e
Katherine Jackson <katherine.jackson206@gmail.com>
2021-07-15 22:40:09 -0500

    changed discover to show search results card

--- lineup/client/src/pages/Discover.js
+++ lineup/client/src/pages/Discover.js
@@ -1,74 +1,79 @@
-// import React, { Component } from "react";
-// import { Card, Alert, Button } from 'react-bootstrap';
+import React from "react";
+import SearchResultsCard from "../components/SearchResultsCard";
+import { Row } from "react-bootstrap";
 // import API from "../utils/API";
 // import axios from "axios";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import './index.css';
 
+function Discover() {
+  // class Discover extends Component {
+  //   state = {
+  //     image: "",
+  //     match: false,
+  //     matchCount: 0
+  //   };
 
-// class Discover extends Component {
-//   state = {
-//     image: "",
-//     match: false,
-//     matchCount: 0
-//   };
+  //   // When the component mounts, load the next dog to be displayed
+  //   componentDidMount() {
+  //     this.loadNextDog();
+  //   }
 
-//   // When the component mounts, load the next dog to be displayed
-//   componentDidMount() {
-//     this.loadNextDog();
-//   }
+  //   handleBtnClick = event => {
+  //     // Get the data-value of the clicked button
+  //     const btnType = event.target.attributes.getNamedItem("data-value").value;
+  //     // Clone this.state to the newState object
+  //     // We'll modify this object and use it to set our component's state
+  //     const newState = { ...this.state };
 
-//   handleBtnClick = event => {
-//     // Get the data-value of the clicked button
-//     const btnType = event.target.attributes.getNamedItem("data-value").value;
-//     // Clone this.state to the newState object
-//     // We'll modify this object and use it to set our component's state
-//     const newState = { ...this.state };
+  //     if (btnType === "pick") {
+  //       // Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
+  //       newState.match = 1 === Math.floor(Math.random() * 5) + 1;
 
-//     if (btnType === "pick") {
-//       // Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
-//       newState.match = 1 === Math.floor(Math.random() * 5) + 1;
+  //       // Set newState.matchCount equal to its current value or its current value + 1 depending on whether the dog likes us
+  //       newState.matchCount = newState.match
+  // ? newState.matchCount + 1
+  //         : newState.matchCount;
+  //     } else {
+  //       // If we thumbs down'ed the dog, we haven't matched with it
+  //       newState.match = false;
+  //     }
+  //     // Replace our component's state with newState, load the next dog image
+  //     this.setState(newState);
+  //     this.loadNextDog();
+  //   };
 
-//       // Set newState.matchCount equal to its current value or its current value + 1 depending on whether the dog likes us
-//       newState.matchCount = newState.match
-//         ? newState.matchCount + 1
-//         : newState.matchCount;
-//     } else {
-//       // If we thumbs down'ed the dog, we haven't matched with it
-//       newState.match = false;
-//     }
-//     // Replace our component's state with newState, load the next dog image
-//     this.setState(newState);
-//     this.loadNextDog();
-//   };
+  //   loadNextDog = () => {
+  //     API.getRandomDog()
+  //       .then(res =>
+  //         this.setState({
+  //           image: res.data.message
+  //         })
+  //       )
+  //       .catch(err => console.log(err));
+  //   };
 
-//   loadNextDog = () => {
-//     API.getRandomDog()
-//       .then(res =>
-//         this.setState({
-//           image: res.data.message
-//         })
-//       )
-//       .catch(err => console.log(err));
-//   };
+  return (
+    <SearchResultsCard></SearchResultsCard>
 
-//   render() {
-//     return (
-//       <div>
-//         <h1 className="text-center">Make New Friends</h1>
-//         <h3 className="text-center">
-//           Thumbs up on any pups you'd like to meet!
-//         </h3>
-//         <Card image={this.state.image} handleBtnClick={this.handleBtnClick} />
-//         <h1 className="text-center">
-//           Made friends with {this.state.matchCount} pups so far!
-//         </h1>
-//         <Alert style={{ opacity: this.state.match ? 1 : 0 }} type="success">
-//           Yay! That Pup Liked You Too!!!
-//         </Alert>
-//       </div>
-//     );
-//   }
-// }
+    // WORKING ON SWIPE RIGHT //
 
-// export default Discover;
+    // const data = ['Alexandre', 'Thomas', 'Lucien']
+
+    // const Wrapper = () => {
+    //   return (
+    // 	  <Cards onEnd={action('end')} className='master-root'>
+    //         {data.map(item =>
+    //           <Card
+    //             onSwipeLeft={action('swipe left')}
+    //             onSwipeRight={action('swipe right')}>
+    //             <h2>{item}</h2>
+    //           </Card>
+    //         )}
+    //       </Cards>
+    //   )
+    // }
+  );
+}
+
+export default Discover;
