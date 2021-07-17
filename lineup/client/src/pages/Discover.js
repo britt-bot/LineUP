9d95776abfbefc46daa85df97348a4c5ba36fdc8
Katherine Jackson <katherine.jackson206@gmail.com>
2021-07-17 12:37:55 -0500

    added carousel - not function fully

--- lineup/client/src/pages/Discover.js
+++ lineup/client/src/pages/Discover.js
@@ -1,60 +1,62 @@
 import React from "react";
-import SearchResultsCard from "../components/SearchResultsCard";
-import { Row } from "react-bootstrap";
+import DiscoverCarousel from "../components/DiscoverCarousel";
+// import SearchResultsCard from "../components/SearchResultsCard";
+// import { Row } from "react-bootstrap";
 // import API from "../utils/API";
 // import axios from "axios";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import './index.css';
 
+// function Discover() {
+// class Discover extends Component {
+//   state = {
+//     image: "",
+//     match: false,
+//     matchCount: 0
+//   };
+
+//   // When the component mounts, load the next dog to be displayed
+//   componentDidMount() {
+//     this.loadNextDog();
+//   }
+
+//   handleBtnClick = event => {
+//     // Get the data-value of the clicked button
+//     const btnType = event.target.attributes.getNamedItem("data-value").value;
+//     // Clone this.state to the newState object
+//     // We'll modify this object and use it to set our component's state
+//     const newState = { ...this.state };
+
+//     if (btnType === "pick") {
+//       // Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
+//       newState.match = 1 === Math.floor(Math.random() * 5) + 1;
+
+//       // Set newState.matchCount equal to its current value or its current value + 1 depending on whether the dog likes us
+//       newState.matchCount = newState.match
+// ? newState.matchCount + 1
+//         : newState.matchCount;
+//     } else {
+//       // If we thumbs down'ed the dog, we haven't matched with it
+//       newState.match = false;
+//     }
+//     // Replace our component's state with newState, load the next dog image
+//     this.setState(newState);
+//     this.loadNextDog();
+//   };
+
+//   loadNextDog = () => {
+//     API.getRandomDog()
+//       .then(res =>
+//         this.setState({
+//           image: res.data.message
+//         })
+//       )
+//       .catch(err => console.log(err));
+//   };
+
 function Discover() {
-  // class Discover extends Component {
-  //   state = {
-  //     image: "",
-  //     match: false,
-  //     matchCount: 0
-  //   };
-
-  //   // When the component mounts, load the next dog to be displayed
-  //   componentDidMount() {
-  //     this.loadNextDog();
-  //   }
-
-  //   handleBtnClick = event => {
-  //     // Get the data-value of the clicked button
-  //     const btnType = event.target.attributes.getNamedItem("data-value").value;
-  //     // Clone this.state to the newState object
-  //     // We'll modify this object and use it to set our component's state
-  //     const newState = { ...this.state };
-
-  //     if (btnType === "pick") {
-  //       // Set newState.match to either true or false depending on whether or not the dog likes us (1/5 chance)
-  //       newState.match = 1 === Math.floor(Math.random() * 5) + 1;
-
-  //       // Set newState.matchCount equal to its current value or its current value + 1 depending on whether the dog likes us
-  //       newState.matchCount = newState.match
-  // ? newState.matchCount + 1
-  //         : newState.matchCount;
-  //     } else {
-  //       // If we thumbs down'ed the dog, we haven't matched with it
-  //       newState.match = false;
-  //     }
-  //     // Replace our component's state with newState, load the next dog image
-  //     this.setState(newState);
-  //     this.loadNextDog();
-  //   };
-
-  //   loadNextDog = () => {
-  //     API.getRandomDog()
-  //       .then(res =>
-  //         this.setState({
-  //           image: res.data.message
-  //         })
-  //       )
-  //       .catch(err => console.log(err));
-  //   };
-
   return (
-    <SearchResultsCard></SearchResultsCard>
+    <DiscoverCarousel />
 
     // WORKING ON SWIPE RIGHT //
 
