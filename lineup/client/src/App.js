d093fb328ebcf7f4045981b318f740e2e0f0021a
Katherine Jackson <katherine.jackson206@gmail.com>
2021-07-17 16:23:30 -0500

    added login/signup routes

--- lineup/client/src/App.js
+++ lineup/client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
 import Discover from "./pages/Discover";
 import Search from "./pages/Search";
 import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 import Users from "./pages/Users";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -18,9 +20,11 @@ function App() {
         <Users />
         <Wrapper>
           <Route exact path="/" component={Homepage} />
+          <Route exact path="/login" component={Login} />
           <Route exact path="/discover" component={Discover} />
           <Route exact path="/search" component={Search} />
           <Route exact path="/profile" component={Profile} />
+          <Route exact path="/signup" component={Signup} />
         </Wrapper>
         <Footer />
       </div>
