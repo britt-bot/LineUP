daa508beb7eb16af5d9eb94eb4ac7371f836a02c
Katherine Jackson <katherine.jackson206@gmail.com>
2021-07-17 12:37:38 -0500

    connected card with search results

--- lineup/client/src/components/SearchBtn/index.js
+++ lineup/client/src/components/SearchBtn/index.js
@@ -3,49 +3,61 @@ import "./style.css";
 import { Button, Form, FormControl } from "react-bootstrap";
 import { useState } from "react";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function Searchbtn() {
-
   const [search, setSearch] = useState("");
+  const history = useHistory();
 
   const handleClick = async (event) => {
     event.preventDefault();
 
-  try {
+    try {
+      const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=${process.env.REACT_APP_EVENT_API_KEY}&keyword=`;
 
-    const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=${process.env.REACT_APP_EVENT_API_KEY}&keyword=`
+      const response = await axios.get(
+        url + search + "&locale=en-us&countryCode=US&segmentName=music&page=1"
+      );
+      let searchResult = response.data._embedded.events;
 
-    const response = await axios.get(url + search + '&locale=en-us&countryCode=US&segmentName=music&page=1');
-    let searchResult = response.data._embedded.events;
+      let filteredEvents = searchResult.filter((events) => {
+        return "name" in events;
+      });
 
-    let filteredEvents = searchResult.filter(events => {
-    return ('name' in events)
-    });
+      let festivals = filteredEvents.map((event) => {
+        return {
+          name: event.name,
+          id: event.id,
+          summary: event.description,
+          ticket: event.url,
+          logo: event.images[event.images.length - 1].url,
+          status: event.dates.status.code,
+          span: event.dates.spanMultipleDays,
+          date: event.dates.start.localDate,
+          start: event.dates.start.localTime,
+          venues: event._embedded.venues[0].name,
+          address: event._embedded.venues[0].address.line1,
+          city: event._embedded.venues[0].city.name,
+          state: event._embedded.venues[0].state.stateCode,
+        };
+      });
 
-    let festivals = filteredEvents.map((event) => {
+      // let resultsPages = [];
+      // for (let index = 1; index <= response.data.page.totalPages; index++) {
+      //   resultsPages.push({
+      //     index: index,
+      //     href: "/results/" + search + "/" + index,
+      //   });
+      // }
 
-      return {
-        name: event.name,
-        id: event.id,
-        summary: event.description,
-        logo: event.images[event.images.length - 1].url,
-        status: event.dates.status.code,
-        span: event.dates.spanMultipleDays,
-        date: event.dates.start.localDate,
-        start: event.dates.start.localTime,
-        venues: event._embedded.venues[0].name,
-        address: event._embedded.venues[0].address.line1,
-        city: event._embedded.venues[0].city.name,
-        state: event._embedded.venues[0].state.stateCode
-      }
-    });
+      // history.push("/search");
+      history.push({
+        pathname: "/search",
+        // search: '?query=abc',
+        state: { festivals },
+      });
 
-    let resultsPages = [];
-    for (let index = 1; index <= response.data.page.totalPages; index++) {
-      resultsPages.push({index:index,href:"/results/"+search+"/"+index});
-    }
-    console.log(festivals);
-
+      console.log(festivals);
     } catch (err) {
       console.log(err);
     }
@@ -72,6 +84,6 @@ function Searchbtn() {
       </Button>
     </Form>
   );
-};
+}
 
 export default Searchbtn;
--- lineup/client/src/components/SearchResultsCard/index.js
+++ lineup/client/src/components/SearchResultsCard/index.js
@@ -4,22 +4,27 @@ import "./style.css";
 import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 
 function SearchResultsCard(props) {
+  // console.log(props);
+  const {
+    festival: { name, summary, logo, status, date, start, venues, ticket },
+  } = props;
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+      <Card.Img variant="top" src={logo} />
       <Card.Body>
-        <Card.Title>Event Name</Card.Title>
-        <Card.Text>Event Summary</Card.Text>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{summary}</Card.Text>
       </Card.Body>
       <ListGroup className="list-group-flush">
-        <ListGroupItem>Event Status</ListGroupItem>
-        <ListGroupItem>Event Date</ListGroupItem>
-        <ListGroupItem>Event Start Time</ListGroupItem>
-        <ListGroupItem>Vanue Address</ListGroupItem>
+        <ListGroupItem>{status}</ListGroupItem>
+        <ListGroupItem>{date}</ListGroupItem>
+        <ListGroupItem>{start}</ListGroupItem>
+        <ListGroupItem>{venues}</ListGroupItem>
       </ListGroup>
       <Card.Body>
-        <Card.Link href="#">Link to Venue</Card.Link>
-        <Card.Link href="#">Link to Buy Tickets</Card.Link>
+        <Card.Link href={ticket} target="_blank">
+          Buy Tickets
+        </Card.Link>
       </Card.Body>
     </Card>
   );
