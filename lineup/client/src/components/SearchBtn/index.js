eb6c5a190f8957711f3e7a6a9afa04161545d226
Jakers811 <akerstjustin@gmail.com>
2021-07-17 10:35:19 -0500

    Added results array with searched stuff

--- lineup/client/src/components/SearchBtn/index.js
+++ lineup/client/src/components/SearchBtn/index.js
@@ -5,22 +5,53 @@ import { useState } from "react";
 import axios from "axios";
 
 function Searchbtn() {
-  console.log(process.env.REACT_APP_EVENT_API_KEY);
+
   const [search, setSearch] = useState("");
 
   const handleClick = async (event) => {
-    console.log("clicked");
     event.preventDefault();
 
-    const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=${process.env.REACT_APP_EVENT_API_KEY}&keyword=`;
+  try {
 
-    const response = await axios.get(
-      url + search + "&locale=en-us&countryCode=US&segmentName=music&page=1"
-    );
+    const url = `https://app.ticketmaster.com/discovery/v2/events?apikey=${process.env.REACT_APP_EVENT_API_KEY}&keyword=`
+
+    const response = await axios.get(url + search + '&locale=en-us&countryCode=US&segmentName=music&page=1');
+    let searchResult = response.data._embedded.events;
+
+    let filteredEvents = searchResult.filter(events => {
+    return ('name' in events)
+    });
+
+    let festivals = filteredEvents.map((event) => {
+
+      return {
+        name: event.name,
+        id: event.id,
+        summary: event.description,
+        logo: event.images[event.images.length - 1].url,
+        status: event.dates.status.code,
+        span: event.dates.spanMultipleDays,
+        date: event.dates.start.localDate,
+        start: event.dates.start.localTime,
+        venues: event._embedded.venues[0].name,
+        address: event._embedded.venues[0].address.line1,
+        city: event._embedded.venues[0].city.name,
+        state: event._embedded.venues[0].state.stateCode
+      }
+    });
+
+    let resultsPages = [];
+    for (let index = 1; index <= response.data.page.totalPages; index++) {
+      resultsPages.push({index:index,href:"/results/"+search+"/"+index});
+    }
+    console.log(festivals);
+
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const handleChange = (event) => {
-    console.log(event.target.value);
     setSearch(event.target.value);
   };
 
@@ -41,6 +72,6 @@ function Searchbtn() {
       </Button>
     </Form>
   );
-}
+};
 
 export default Searchbtn;
