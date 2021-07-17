a06724d2410fca1c169ca15aa8c89cfee1a274fa
Katherine Jackson <katherine.jackson206@gmail.com>
2021-07-17 14:40:53 -0500

    added onclick color change

--- lineup/client/src/components/FavoriteBtn/index.js
+++ lineup/client/src/components/FavoriteBtn/index.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 function FavoriteBtn() {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const handleClick = () => setIsFavorite(!isFavorite);
+
   return (
     <Button
+      style={{ color: isFavorite ? "#FFD700" : "black" }}
       className="favBtn"
       variant="outline-transparent"
-      // onClick=
+      onClick={handleClick}
     >
       <ion-icon name="star-outline"></ion-icon>
     </Button>
