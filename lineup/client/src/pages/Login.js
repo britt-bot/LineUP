51cf110852d13e424584aa00e1ce3e9d2a1f72fc
Katherine Jackson <katherine.jackson206@gmail.com>
2021-07-22 22:38:27 -0500

    merged to main

--- lineup/client/src/pages/Login.js
+++ lineup/client/src/pages/Login.js
@@ -1,8 +1,18 @@
 import React from "react";
 import LoginForm from "../components/LoginForm";
+import { Card, Tab, Tabs } from "@blueprintjs/core";
+import { useState } from "react";
+import Login from "../components/LoginForm";
+import Signup from "../components/SignupForm";
 
-function Login() {
-  return <LoginForm />;
+function LoginScreen() {
+  const [currentTab, setCurrentTab] = useState("login");
+  return (<Card elevation="1">
+    <Tabs id="Tabs" onChange={setCurrentTab} selectedTabId={currentTab}>
+      <Tab id="login" title="Login" panel={<Login />} />
+      <Tab id="Signup" title="Signup" panel={<Signup />} />
+      <Tabs.Expander />
+    </Tabs>
+  </Card>)(<LoginForm />);
 }
-
-export default Login;
+export default LoginScreen;
