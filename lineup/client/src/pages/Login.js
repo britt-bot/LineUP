0b4c3d3fae192766ac2e06f4205652ab05c2b7b5
Katherine Jackson <katherine.jackson206@gmail.com>
2021-07-24 12:33:59 -0500

    added tabs and deleted unused import

--- lineup/client/src/pages/Login.js
+++ lineup/client/src/pages/Login.js
@@ -1,5 +1,4 @@
 import React from "react";
-import LoginForm from "../components/LoginForm";
 import { Card, Tab, Tabs } from "@blueprintjs/core";
 import { useState } from "react";
 import Login from "../components/LoginForm";
@@ -7,12 +6,14 @@ import Signup from "../components/SignupForm";
 
 function LoginScreen() {
   const [currentTab, setCurrentTab] = useState("login");
-  return (<Card elevation="1">
-    <Tabs id="Tabs" onChange={setCurrentTab} selectedTabId={currentTab}>
-      <Tab id="login" title="Login" panel={<Login />} />
-      <Tab id="Signup" title="Signup" panel={<Signup />} />
-      <Tabs.Expander />
-    </Tabs>
-  </Card>)(<LoginForm />);
+  return (
+    <Card elevation="1">
+      <Tabs id="Tabs" onChange={setCurrentTab} selectedTabId={currentTab}>
+        <Tab id="login" title="Login" panel={<Login />} />
+        <Tab id="Signup" title="Signup" panel={<Signup />} />
+        <Tabs.Expander />
+      </Tabs>
+    </Card>
+  );
 }
 export default LoginScreen;
