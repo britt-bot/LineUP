e2c96ce693042d20f48c7ac026a7d8446f0b3b56
mmorrisonlk <mmorrisonlk@gmail.com>
2021-07-24 13:52:19 -0500

    Added base favorites

--- lineup/client/src/utils/API.js
+++ lineup/client/src/utils/API.js
@@ -14,4 +14,9 @@ export default {
         console.log(data);
     return axios.post("/api/user/login", data);
   },
+  favoriteSave: function (data) {
+    console.log("axios.favorite");
+    console.log(data);
+    return axios.post("/api/user/favorite", data);
+},
 };
