4890eeb1237333718e0e479b4a4d8981588ecb19
Katherine Jackson <katherine.jackson206@gmail.com>
2021-07-24 14:48:35 -0500

    updated favorite not working but code jared helped

--- lineup/client/src/utils/API.js
+++ lineup/client/src/utils/API.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 
 export default {
-  getUsers: function () {
-    return axios.get("/api/user");
+  getUser: function () {
+    return axios.get("/api/user/me");
   },
-    createUser: function (data) {
-        console.log("axios.create");
-        console.log(data);
+  createUser: function (data) {
+    console.log("axios.create");
+    console.log(data);
     return axios.post("/api/user/add", data);
   },
-    loginForm: function (data) {
-        console.log("axios.login");
-        console.log(data);
+  loginForm: function (data) {
+    console.log("axios.login");
+    console.log(data);
     return axios.post("/api/user/login", data);
   },
   favoriteSave: function (data) {
     console.log("axios.favorite");
     console.log(data);
     return axios.post("/api/user/favorite", data);
-},
+  },
 };
