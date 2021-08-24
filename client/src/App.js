5b5d0957fb1b8ace97458dc133cd68465ca7e7d9
mmorrisonlk <mmorrisonlk@gmail.com>
2021-08-23 22:02:13 -0500

    Cleaned up various error and warning messages

--- client/src/App.js
+++ client/src/App.js
@@ -4,31 +4,29 @@ import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import Discover from "./pages/Discover";
 import Search from "./pages/Search";
-import Profile from "./pages/Profile";
+// import Profile from "./pages/Profile";
 import Signup from "./pages/Signup";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import Navbar from "./components/Navbar";
 import "./App.css";
-import { UserProvider } from "./context/UserContext";
+// import { UserProvider } from "./context/UserContext";
 
 function App() {
   return (
     <Router>
-      <div>
         <Navbar />
         <Wrapper>
           <Route exact path="/" component={Homepage} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/discover" component={Discover} />
           <Route exact path="/search" component={Search} />
-          <UserProvider>
+          {/* <UserProvider>
             <Route exact path="/profile" component={Profile} />
-          </UserProvider>
+          </UserProvider> */}
           <Route exact path="/signup" component={Signup} />
         </Wrapper>
         <Footer />
-      </div>
     </Router>
   );
 }
